27d2aca95d8689becc95317f66106a2fa0502249
Curtis Bishop <33741939+cbishop76@users.noreply.github.com>
2020-01-02 13:33:02 -0700

    Update cmd_pmode.py

--- pyvizio/cmd_pmode.py
+++ pyvizio/cmd_pmode.py
@@ -1,7 +1,7 @@
 from .protocol import get_json_obj, ProtoConstants, InfoCommandBase, CommandBase, Endpoints
 
 
-class VizioInput(object):
+class VizioPmode(object):
     def __init__(self, json_item, is_extended_metadata):
         self.id = int(get_json_obj(json_item, ProtoConstants.Item.HASHVAL))
         self.c_name = get_json_obj(json_item, ProtoConstants.Item.CNAME)
@@ -22,50 +22,50 @@ class VizioInput(object):
             self.meta_name = self.c_name
 
 
-class GetInputsListCommand(InfoCommandBase):
-    """Obtaining list of available inputs"""
+class GetPmodesListCommand(InfoCommandBase):
+    """Obtaining list of available picture modes"""
 
     def __init__(self, device_type):
-        super(GetInputsListCommand, self).__init__()
-        InfoCommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["INPUTS"])
+        super(GetPmodesListCommand, self).__init__()
+        InfoCommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["PMODES"])
         self._device_type = device_type
 
     def process_response(self, json_obj):
         items = get_json_obj(json_obj, ProtoConstants.RESPONSE_ITEMS)
         
         # Last input for sound bar is the current input so it needs to be removed before processing
-        if self._device_type == "soundbar":
-            items = items[:-1]
+#        if self._device_type == "soundbar":
+#            items = items[:-1]
 
-        inputs = []
+        pmodes = []
 
         if items is not None:
             for itm in items:
-                v_input = VizioInput(itm, True)
-                inputs.append(v_input)
+                v_pmode = VizioPmode(itm, True)
+                pmodes.append(v_pmode)
 
-        return inputs
+        return pmodes
 
 
-class GetCurrentInputCommand(InfoCommandBase):
-    """Obtaining current input"""
+class GetCurrentPmodeCommand(InfoCommandBase):
+    """Obtaining current picture mode"""
 
     def __init__(self, device_type):
-        super(GetCurrentInputCommand, self).__init__()
-        InfoCommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["CURR_INPUT"])
+        super(GetCurrentPmodeCommand, self).__init__()
+        InfoCommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["CURR_PMODE"])
 
     def process_response(self, json_obj):
         items = get_json_obj(json_obj, ProtoConstants.RESPONSE_ITEMS)
-        v_input = None
+        v_pmode = None
         if len(items) > 0:
-            v_input = VizioInput(items[0], False)
-        return v_input
+            v_pmode = VizioPmode(items[0], False)
+        return v_pmode
 
 
-class ChangeInputCommand(CommandBase):
+class ChangePmodeCommand(CommandBase):
     def __init__(self, id_, name, device_type):
-        super(ChangeInputCommand, self).__init__()
-        CommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["SET_INPUT"])
+        super(ChangePmodeCommand, self).__init__()
+        CommandBase.url.fset(self, Endpoints.ENDPOINTS[device_type]["SET_PMODE"])
         self.VALUE = str(name)
         # noinspection SpellCheckingInspection
         self.HASHVAL = int(id_)
