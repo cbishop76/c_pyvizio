076dc2f604e62b7339c07822cd6ea1f66f0353a5
Curtis Bishop <33741939+cbishop76@users.noreply.github.com>
2020-01-03 11:03:13 -0700

    Update and rename pyvizio/cli.py to c_pyvizio/cli.py

--- c_pyvizio/cli.py
+++ c_pyvizio/cli.py
@@ -1,7 +1,7 @@
 import logging
 import click
 import sys
-import pyvizio
+import c_pyvizio
 
 if sys.version_info < (3, 4):
     print("To use this script you need python 3.4 or newer, got %s" %
@@ -9,10 +9,10 @@ if sys.version_info < (3, 4):
     sys.exit(1)
 
 _LOGGER = logging.getLogger(__name__)
-DEVICE_ID = "cpyvizio"
-DEVICE_NAME = "Python Vizio C"
+DEVICE_ID = "c_pyvizio"
+DEVICE_NAME = "C Python Vizio"
 
-pass_vizio = click.make_pass_decorator(pyvizio.Vizio)
+pass_vizio = click.make_pass_decorator(c_pyvizio.c_Vizio)
 
 
 @click.group(invoke_without_command=False)
@@ -28,7 +28,7 @@ def cli(ctx, ip, auth, device_type):
 @cli.command()
 def discover():
     logging.basicConfig(level=logging.INFO)
-    devices = pyvizio.Vizio.discovery()
+    devices = c_pyvizio.c_Vizio.discovery()
     log_data = "Available devices:" \
                "\nIP\tModel\tFriendly name"
     for dev in devices:
@@ -38,9 +38,9 @@ def discover():
 
 @cli.command()
 @pass_vizio
-def pair(vizio):
+def pair(c_vizio):
     _LOGGER.info("Initiating pairing process, please check your device for pin upon success")
-    pair_data = vizio.start_pair()
+    pair_data = c_vizio.start_pair()
     if pair_data is not None:
         _LOGGER.info("Challenge type: %s", pair_data.ch_type)
         _LOGGER.info("Challenge token: %s", pair_data.token)
@@ -48,9 +48,9 @@ def pair(vizio):
 
 @cli.command()
 @pass_vizio
-def pair_stop(vizio):
+def pair_stop(c_vizio):
     _LOGGER.info("Sending stop pair command")
-    vizio.stop_pair()
+    c_vizio.stop_pair()
 
 
 @cli.command()
@@ -58,17 +58,17 @@ def pair_stop(vizio):
 @click.option('--token', required=True, help="Challenge token obtained from pair command")
 @click.option('--pin', required=True, help="PIN obtained from device after running pair command")
 @pass_vizio
-def pair_finish(vizio, ch_type, token, pin):
+def pair_finish(c_vizio, ch_type, token, pin):
     _LOGGER.info("Finishing pairing")
-    pair_data = vizio.pair(ch_type, token, pin)
+    pair_data = c_vizio.pair(ch_type, token, pin)
     if pair_data is not None:
         _LOGGER.info("Authorization token: %s", pair_data.auth_token)
 
 
 @cli.command()
 @pass_vizio
-def input_list(vizio):
-    inputs = vizio.get_inputs()
+def input_list(c_vizio):
+    inputs = c_vizio.get_inputs()
     if inputs:
         log_data = "Available inputs:" \
                 "\nName\tFriendly name\tType\tID"
@@ -82,8 +82,8 @@ def input_list(vizio):
 
 @cli.command()
 @pass_vizio
-def pmode_list(vizio):
-    pmodes = vizio.get_pmodes()
+def pmode_list(c_vizio):
+    pmodes = c_vizio.get_pmodes()
     if pmodes:
         log_data = "Available picture modes:" \
                 "\nName\tFriendly name\tType\tID"
@@ -97,8 +97,8 @@ def pmode_list(vizio):
 
 @cli.command()
 @pass_vizio
-def input_get(vizio):
-    data = vizio.get_current_input()
+def input_get(c_vizio):
+    data = c_vizio.get_current_input()
     if data:
         _LOGGER.info("Current input: %s", data.meta_name)
     else:
@@ -107,8 +107,8 @@ def input_get(vizio):
 
 @cli.command()
 @pass_vizio
-def pmode_get(vizio):
-    data = vizio.get_current_pmode()
+def pmode_get(c_vizio):
+    data = c_vizio.get_current_pmode()
     if data:
         _LOGGER.info("Current picture mode: %s", data.meta_name)
     else:
@@ -117,24 +117,24 @@ def pmode_get(vizio):
 
 @cli.command()
 @pass_vizio
-def power_get(vizio):
-    is_on = vizio.get_power_state()
+def power_get(c_vizio):
+    is_on = c_vizio.get_power_state()
     _LOGGER.info("Device is on" if is_on else "Device is off")
 
 
 @cli.command()
 @click.argument("state", required=False, default="toggle", type=click.Choice(["toggle", "on", "off"]))
 @pass_vizio
-def power(vizio, state):
+def power(c_vizio, state):
     if "on" == state:
         txt = "Turning ON"
-        result = vizio.pow_on()
+        result = c_vizio.pow_on()
     elif "off" == state:
         txt = "Turning OFF"
-        result = vizio.pow_off()
+        result = c_vizio.pow_off()
     else:
         txt = "Toggling power"
-        result = vizio.pow_toggle()
+        result = c_vizio.pow_toggle()
     _LOGGER.info(txt)
     _LOGGER.info("OK" if result else "ERROR")
 
@@ -143,45 +143,45 @@ def power(vizio, state):
 @click.argument("state", required=False, default="up", type=click.Choice(["up", "down"]))
 @click.argument("amount", required=False, default=1, type=click.IntRange(1, 100, clamp=True))
 @pass_vizio
-def volume(vizio, state, amount):
+def volume(c_vizio, state, amount):
     amount = int(amount)
     if "up" == state:
         txt = "Increasing volume"
-        result = vizio.vol_up(amount)
+        result = c_vizio.vol_up(amount)
     else:
         txt = "Decreasing volume"
-        result = vizio.vol_down(amount)
+        result = c_vizio.vol_down(amount)
     _LOGGER.info(txt)
     _LOGGER.info("OK" if result else "ERROR")
 
 
 @cli.command()
 @pass_vizio
-def volume_current(vizio):
-    _LOGGER.info("Current volume: %s", vizio.get_current_volume())
+def volume_current(c_vizio):
+    _LOGGER.info("Current volume: %s", c_vizio.get_current_volume())
 
 
 @cli.command()
 @pass_vizio
-def volume_max(vizio):
-    _LOGGER.info("Max volume: %s", vizio.get_max_volume())
+def volume_max(c_vizio):
+    _LOGGER.info("Max volume: %s", c_vizio.get_max_volume())
 
 
 @cli.command()
 @click.argument("state", required=False, default="previous", type=click.Choice(["up", "down", "previous"]))
 @click.argument("amount", required=False, default=1, type=click.IntRange(1, 100, clamp=True))
 @pass_vizio
-def channel(vizio, state, amount):
+def channel(c_vizio, state, amount):
     amount = int(amount)
     if "up" == state:
         txt = "Channel up"
-        result = vizio.ch_up(amount)
+        result = c_vizio.ch_up(amount)
     elif "down" == state:
         txt = "Channel down"
-        result = vizio.ch_down(amount)
+        result = c_vizio.ch_down(amount)
     else:
         txt = "Previous channel"
-        result = vizio.ch_prev()
+        result = c_vizio.ch_prev()
     _LOGGER.info(txt)
     _LOGGER.info("OK" if result else "ERROR")
 
@@ -189,24 +189,24 @@ def channel(vizio, state, amount):
 @cli.command()
 @click.argument("state", required=False, default="toggle", type=click.Choice(["toggle", "on", "off"]))
 @pass_vizio
-def mute(vizio, state):
+def mute(c_vizio, state):
     if "on" == state:
         txt = "Muting"
-        result = vizio.mute_on()
+        result = c_vizio.mute_on()
     elif "off" == state:
         txt = "Un-muting"
-        result = vizio.mute_off()
+        result = c_vizio.mute_off()
     else:
         txt = "Toggling mute"
-        result = vizio.mute_toggle()
+        result = c_vizio.mute_toggle()
     _LOGGER.info(txt)
     _LOGGER.info("OK" if result else "ERROR")
 
 
 @cli.command()
 @pass_vizio
-def input_next(vizio):
-    result = vizio.input_next()
+def input_next(c_vizio):
+    result = c_vizio.input_next()
     _LOGGER.info("Circling input")
     _LOGGER.info("OK" if result else "ERROR")
 
@@ -214,8 +214,8 @@ def input_next(vizio):
 @cli.command(name="input")
 @click.option('--name', required=True)
 @pass_vizio
-def input(vizio, name):
-    result = vizio.input_switch(name)
+def input(c_vizio, name):
+    result = c_vizio.input_switch(name)
     _LOGGER.info("Switching input")
     _LOGGER.info("OK" if result else "ERROR")
 
@@ -223,32 +223,32 @@ def input(vizio, name):
 @cli.command(name="pmode")
 @click.option('--name', required=True)
 @pass_vizio
-def pmode(vizio, name):
-    result = vizio.pmode_switch(name)
+def pmode(c_vizio, name):
+    result = c_vizio.pmode_switch(name)
     _LOGGER.info("Switching picture mode")
     _LOGGER.info("OK" if result else "ERROR")
 
 
 @cli.command()
 @pass_vizio
-def play(vizio):
-    result = vizio.play()
+def play(c_vizio):
+    result = c_vizio.play()
     _LOGGER.info("OK" if result else "ERROR")
 
 
 @cli.command()
 @pass_vizio
-def pause(vizio):
-    result = vizio.pause()
+def pause(c_vizio):
+    result = c_vizio.pause()
     _LOGGER.info("OK" if result else "ERROR")
 
 
 @cli.command()
 @click.argument("key", required=True)
 @pass_vizio
-def key_press(vizio, key):
+def key_press(c_vizio, key):
     _LOGGER.info("Emulating pressing of '%s' key", key)
-    result = vizio.remote(key)
+    result = c_vizio.remote(key)
     _LOGGER.info("OK" if result else "ERROR")
 
 
