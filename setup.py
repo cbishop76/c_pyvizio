bffd5b237628ed35e93fbf9a2fe0057a4627c6c1
Curtis Bishop <33741939+cbishop76@users.noreply.github.com>
2020-01-03 11:13:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
-with open('pyvizio/version.py') as f: exec(f.read())
+with open('c_pyvizio/version.py') as f: exec(f.read())
 with open('README.md', 'r') as myfile:
     longdescription=myfile.read()
 
 setup(
-    name='pyvizio',
+    name='c_pyvizio',
 
     version=__version__,
     description='Python library for interfacing with Vizio SmartCast TVs and Sound Bars (2016+ models)',
     long_description=longdescription,
     long_description_content_type="text/markdown",
-    url='https://github.com/vkorn/pyvizio',
+    url='https://github.com/cbishop76/c_pyvizio',
 
     author='Vlad Korniev',
     author_email='vladimir.kornev@gmail.com',
@@ -27,12 +27,12 @@ setup(
 
     keywords='vizio smartcast',
 
-    packages=["pyvizio"],
+    packages=["c_pyvizio"],
 
     install_requires=['click', 'requests', 'jsonpickle', 'xmltodict'],
     entry_points={
         'console_scripts': [
-            'pyvizio=pyvizio.cli:cli',
+            'c_pyvizio=c_pyvizio.cli:cli',
         ],
     },
 )
